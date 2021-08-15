982291564581ed7ab241ba892d4115f6db40e6ee
NikhilMugganawar <46456302+NikhilMugganawar@users.noreply.github.com>
2021-08-15 20:23:41 +0530

    Update assigment1_3.R
    Added function for finding sumof_n_numbers

--- lab1/assigment1_3.R
+++ lab1/assigment1_3.R
@@ -7,3 +7,19 @@ maximum=function(a,b,c)
     max=c;
   return(max);
 }
+
+sumof_n_numbers=function(num)
+{if(num < 0){
+  print("Enter a valid number")
+}
+  else{
+    print("It is a valid number")
+    sum = 0
+    while (num > 0) {
+      sum = sum + num
+      num = num -1
+    }
+    print(paste("The sum is", sum))
+  }
+}
+
